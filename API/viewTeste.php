1f10066e5772f1789ec150a4ebef6db7d093b2ca
Polkshot <igoreca221@gmail.com>
2017-08-05 15:17:58 -0300

    Ajuste
    funciona

--- API/viewTeste.php
+++ API/viewTeste.php
@@ -1,7 +1,6 @@
 <?php
 	header('Content-Type: application/json');
 	include_once "../Usuario/controleUsuario.class.php";
-	$data = json_decode(file_get_contents("php://input"));
 	$controle = new controleUsuario();
 	$usuarios = $controle->listar();
 	print_r(json_encode($usuarios, JSON_FORCE_OBJECT));	
